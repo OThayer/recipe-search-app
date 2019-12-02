4b616dbd06933c41b62acb7f90a5d5c780b0163c
OThayer <oliverthayer01@gmail.com>
2019-12-02 16:58:21 -0500

    added initial API interaction

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () =>{
+  const App_ID = "eb3db378";
+  const App_Key = "19ebaa5417834093e78c45950469e081";
+
+  useEffect( () => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${App_ID}&app_key=${App_Key}`)
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input type="text" className="search-bar"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
-
 export default App;
